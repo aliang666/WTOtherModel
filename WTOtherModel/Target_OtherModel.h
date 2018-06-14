2c93cabedeb60bf1799fe503483f596b04d09a41
elji <elji@iflytek.com>
2018-06-14 16:32:01 +0800

    1
    1

---
+++ WTOtherModel/Target_OtherModel.h
@@ -0,0 +1,13 @@
+//
+//  Target_WebViewModel.h
+//  web
+//
+//  Created by elji on 2018/2/9.
+//  Copyright © 2018年 elji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Target_OtherModel : NSObject
+-(void)Action_CameraVC:(NSDictionary*)para;
+@end
---
+++ WTOtherModel/Target_OtherModel.m
@@ -0,0 +1,18 @@
+//
+//  WebView_Action.m
+//
+//
+//  Created by elji on 2018/2/9.
+//  Copyright © 2018年 elji. All rights reserved.
+//
+
+#import "Target_OtherModel.h"
+#import "WTCameraViewController.h"
+#import "WTDefine.h"
+#import "WTUtil.h"
+@implementation Target_WebViewModel
+-(void)Action_CameraVC:(NSDictionary*)para {
+    WTCameraViewController *camera = [[WTCameraViewController alloc] init];
+    WTRootNavPush(camera);
+}
+@end
